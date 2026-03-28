b8d9ac938acdb715ae0a4862cee03091413abec1
Andrés Castañeda <andfcast@hotmail.com>
2026-03-27 22:12:39 -0500

    Montaje de servicio funcional con Swagger

---
+++ InventariosApp/InventariosWebApp/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger(); // Enable Swagger middleware
+    app.UseSwaggerUI(); // Enable Swagger UI middleware
+
+    app.UseSwaggerUI(c => { 
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "InventariosWebApp API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
